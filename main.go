837d7ce90ac02020d87c2cd40794909e436ba414
Mohamad Youness <mehdi@Mohamads-Laptop.netis.cc>
2024-06-08 13:12:30 +0300

    Added full messaging functionality

--- main.go
+++ main.go
@@ -24,20 +24,33 @@ func main() {
 	})
 
 	r.Use(corsHandler(c))
+	// User
 	r.POST("/User", controllers.SignUp)
 	r.POST("/SignIn", controllers.Login)
 	r.GET("/IsAuthenticated", middleware.RequireAuth, controllers.IsLoggedIn)
 	r.GET("/Logout", middleware.RequireAuth, controllers.Logout)
-	r.POST("/Person", controllers.AddPerson)
-	r.GET("/UserId", controllers.GetUserId)
 	r.GET("/User/:id", controllers.GetUser)
 	r.POST("/User/Exists", controllers.DoesUserExist)
+	r.GET("/UserId", controllers.GetUserId)
+
+	// People
+	r.POST("/Person", controllers.AddPerson)
 	r.GET("/Person/:id", controllers.GetPerson)
+
+	// Country
 	r.GET("/Countries", controllers.GetAllCountries)
 	r.POST("/GetCountryWithName", controllers.GetCountryIdWithName)
 	r.GET("/Country/:id", controllers.GetCountryNameWithId)
+
+	// Image
 	r.POST("/Image", controllers.AddImage)
-	r.GET("/LoadMessages/:id", controllers.LoadMessages)
+
+	// Message
+	r.GET("/LoadContacts/:id", controllers.LoadContacts)
+	r.POST("/LoadMessages", controllers.LoadMessages)
+	r.GET("/MessageInfo/:id", controllers.GetMessageInfoFromId)
+	r.POST("/Message", controllers.SendMessage)
+
 	r.Run()
 
 }
