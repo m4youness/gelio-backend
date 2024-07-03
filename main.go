e83c812c416c2d6fb45fce116d1d31872cae2825
Mohamad Youness <mehdi@Mohamads-Laptop.local>
2024-07-03 14:18:21 +0300

    .

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"gelio/m/controllers"
 	"gelio/m/initializers"
 	"gelio/m/middleware"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/cors"
@@ -69,7 +71,13 @@ func main() {
 	r.GET("/Comments/:id", middleware.RequireAuth, controllers.GetComments)
 	r.POST("/Comment", middleware.RequireAuth, controllers.AddComment)
 
-	r.Run()
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	r.Run("0.0.0.0:" + port)
 
 }
 
