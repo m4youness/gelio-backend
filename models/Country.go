a377464114b787b64f5374e40c7451c770d4c19a
Mohamad Youness <mehdi@Mohamads-Laptop.local>
2024-07-11 01:48:34 +0300

    .

--- models/Country.go
+++ models/Country.go
@@ -1,7 +1,6 @@
 package models
 
 type Country struct {
-	CountryID   int     `db:"country_id"`
-	CountryName string  `db:"country_name"`
-	Iso3        *string `db:"iso3"`
+	CountryID   int    `db:"country_id"`
+	CountryName string `db:"country_name"`
 }
