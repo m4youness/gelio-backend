5a85e9fe816302b09f7980da031aefefe0df2774
Mohamad Youness <mehdi@Mohamads-Laptop.local>
2024-07-11 02:24:42 +0300

    .

--- initializers/Database.go
+++ initializers/Database.go
@@ -13,10 +13,17 @@ func DbConnect() {
 	var Err error
 
 	dsn := os.Getenv("DB_URL")
-	DB, Err = sqlx.Connect("postgres", dsn)
-
-	if Err != nil {
-		log.Fatal(Err)
+	if dsn == "" {
+		log.Fatal("DB_URL environment variable not set")
 		return
 	}
+	log.Printf("Connecting to database with DSN: %s", dsn)
+
+	DB, Err = sqlx.Connect("postgres", dsn)
+	if Err != nil {
+		log.Fatalf("Failed to connect to the database: %v", Err)
+		return
+	}
+
+	log.Println("Successfully connected to the database")
 }
